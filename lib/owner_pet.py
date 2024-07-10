9f8c356c24d3fc3461ef193aba31bf54ebf61998
Barasa <timbaros87@gmail.com>
2024-07-10 22:14:17 +0300

    class variable

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,12 +1,15 @@
 class Pet:
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
 
+    all = []
+
     def __init__(self, name, pet_type, owner=None):
         if pet_type not in Pet.PET_TYPES:
             raise Exception(f"Invalid pet type: {pet_type}. Allowed types are: {', '.join(Pet.PET_TYPES)}")
         self.name = name
         self.pet_type = pet_type
         self.owner = owner
+        Pet.all.append(self)
 
 
 class Owner:
